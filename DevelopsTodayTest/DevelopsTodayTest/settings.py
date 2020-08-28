d1286879a4e855b8ca18381c09846ad93e8bbb79
AndriiKret <andrii.kret@gmail.com>
2020-08-28 10:15:51 +0300

    add database conf

--- DevelopsTodayTest/DevelopsTodayTest/settings.py
+++ DevelopsTodayTest/DevelopsTodayTest/settings.py
@@ -73,12 +73,12 @@ WSGI_APPLICATION = 'DevelopsTodayTest.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
@@ -118,3 +118,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
